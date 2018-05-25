cd685f6444562bde220967e1bfce6ea82178d08c
kolinwei <331911734@qq.com>
2018-05-25 17:03:56 +0800

    change ipport

--- tensorflow_benchmark/distributed_replicated/ps_4_worker_4/start_worker3.sh
+++ tensorflow_benchmark/distributed_replicated/ps_4_worker_4/start_worker3.sh
@@ -1,6 +1,6 @@
 python tf_cnn_benchmarks.py --local_parameter_device=gpu --num_gpus=8 \
 --batch_size=64 --model=resnet50 --variable_update=distributed_replicated \
 --job_name=worker \
---ps_hosts=10.255.118.26:5000,10.255.120.16:5000,10.255.123.15:5000,10.255.123.16:5000 \
---worker_hosts=10.255.118.26:5001,10.255.120.16:5001,10.255.123.15:5001,10.255.123.16:5001 \
+--ps_hosts=127.0.0.1:5000,127.0.0.2:5000,127.0.0.3:5000,127.0.0.4:5000 \
+--worker_hosts=127.0.0.1:5001,127.0.0.2:5001,127.0.0.3:5001,127.0.0.4:5001 \
 --task_index=3 
