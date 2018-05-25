7795b062dce8d1904e5f087beb6d9e01d0934850
kolinwei <331911734@qq.com>
2018-05-25 16:59:37 +0800

    add script

---
+++ tensorflow_benchmark/distributed_replicated/ps_1_worker_4/start_worker2.sh
@@ -0,0 +1,5 @@
+python tf_cnn_benchmarks.py --local_parameter_device=gpu --num_gpus=8 \
+--batch_size=64 --model=resnet50 --variable_update=distributed_replicated \
+--job_name=worker --ps_hosts=10.255.118.26:5000 \
+--worker_hosts=10.255.118.26:5001,10.255.120.16:5001,10.255.123.15:5001,10.255.123.16:5001 \
+--task_index=2
